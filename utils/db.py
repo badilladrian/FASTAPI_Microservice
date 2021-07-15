7c22ee3d0528c71729560c911e812fe86cbef10e
Carlos Herrera <carloshv93@gmail.com>
2021-07-15 10:19:02 -0600

    database connection with sqlalchemy

--- utils/db.py
+++ utils/db.py
@@ -1,43 +1,7 @@
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-import psycopg2
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-
-class Database():
-    _instance = None
-    _parameters: str
-
-    def __init__(self, parameters: str = f"host={os.getenv('DB_HOST')} dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASSWORD')} port={os.getenv('DB_PORT')}"):
-        if Database._instance != None:
-            raise Exception("There is another database instace running")
-        else:
-            print("creating database")
-            Database._instance = self
-            self._parameters = parameters
-
-    @staticmethod
-    def getInstance():
-        if Database._instance == None:
-            Database()
-        return Database._instance
-
-    def execute(self, sql_query):
-        connection = psycopg2.connect(self._parameters)
-        cursor = connection.cursor()
-        result = self.query(cursor, sql_query)
-        self.close(cursor, connection)
-        return result
-
-    def query(self, cursor, sql_query):
-        cursor.execute(sql_query)
-        return cursor.fetchall()
-
-    def close(self, cursor, connection):
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+engine = create_engine(
+    'postgresql://farmly_user:farmly_password@localhost:5432/farmly',
+)
+session = Session(engine)
