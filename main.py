a48f09895535bbcc696101ca37d78d844d0d0871
Carlos Herrera <carloshv93@gmail.com>
2021-07-29 07:36:33 -0600

    refactor API

--- main.py
+++ main.py
@@ -23,32 +23,32 @@ plants_controller = ControllerPlants()
 
 tags_metadata = [
     {
-        "name": "Farmly",
-        "description": "All general operations",
+        'name': 'Farmly',
+        'description': 'All general operations',
     },
     {
-        "name": "Yards",
-        "description": "Manage your yards, all yard's operations are here",
+        'name': 'Yards',
+        'description': "Manage your yards, all yard's operations are here",
     },
     {
-        "name": "Beds",
-        "description": "Manage your beds, all bed's operations are here",
+        'name': 'Beds',
+        'description': "Manage your beds, all bed's operations are here",
     },
     {
-        "name": "Plants",
-        "description": "Manage your plants, all plant's operations are here",
-    }
+        'name': 'Plants',
+        'description': "Manage your plants, all plant's operations are here",
+    },
 ]
 
 app = FastAPI(
-    title="Farmly",
-    description="The best assistant for farmers",
-    version="0.0.1",
-    openapi_tags=tags_metadata
+    title='Farmly',
+    description='The best assistant for farmers',
+    version='0.0.1',
+    openapi_tags=tags_metadata,
 )
 
 
-@app.get('/', tags=["Farmly"])
+@app.get('/', tags=['Farmly'])
 async def welcome():
     response = {
         'message': 'Welcome to the best farmer assistant app ever!',
@@ -62,10 +62,10 @@ async def welcome():
 
 # POST
 # Create a new Yard
-@app.post('/yard', tags=["Yards"])
+@app.post('/yard', tags=['Yards'])
 def create_yard(request: list[YardRequestCreate]):
     response = {
-        'message': 'Yards couldn´t be created'
+        'message': 'Yards couldn´t be created',
     }
     yards = yards_controller.create(request=request)
     if yards:
@@ -76,10 +76,10 @@ def create_yard(request: list[YardRequestCreate]):
 
 # GET
 # Get a all and multi yards
-@app.get('/yards', tags=["Yards"])
+@app.get('/yards', tags=['Yards'])
 async def get_yards(id: Optional[List[int]] = Query(None)):
     response = {
-        'message': 'Yards not found'
+        'message': 'Yards not found',
     }
     yards = yards_controller.get_multi(id)
     if yards:
@@ -89,10 +89,10 @@ async def get_yards(id: Optional[List[int]] = Query(None)):
 
 
 # Get a single yard
-@app.get('/yard', tags=["Yards"])
+@app.get('/yard', tags=['Yards'])
 async def get_yard(id: int):
     response = {
-        'message': 'Yard not found'
+        'message': 'Yard not found',
     }
     yard = yards_controller.get_one(id)
     if yard:
@@ -103,10 +103,10 @@ async def get_yard(id: int):
 
 # DELETE
 # Remove a single yard
-@app.delete('/yard', tags=["Yards"])
+@app.delete('/yard', tags=['Yards'])
 def delete_yard(id: int):
     response = {
-        'message': 'Yard does not exist'
+        'message': 'Yard does not exist',
     }
     if yards_controller.delete(id):
         response['message'] = 'Yard successfully deleted'
@@ -116,10 +116,10 @@ def delete_yard(id: int):
 # Update a single yard
 
 
-@app.put('/yard', tags=["Yards"])
+@app.put('/yard', tags=['Yards'])
 def update_yard(id: int, request: YardRequestUpdate):
     response = {
-        'message': 'Yard not found'
+        'message': 'Yard not found',
     }
     if yards_controller.update(id, request):
         response['message'] = 'Yard successfully updated'
@@ -131,65 +131,65 @@ def update_yard(id: int, request: YardRequestUpdate):
 
 # GET
 # Get all and multi beds
-@app.get('/beds', tags=["Beds"])
+@app.get('/beds', tags=['Beds'])
 async def get_beds(id: Optional[List[int]] = Query(None)):
-    message = 'Beds not found'
+    response = {
+        'message': 'Beds not found',
+    }
     beds = beds_controller.get_multi(id)
-    if (len(beds) > 0):
-        message = 'Listing beds'
-    return ({
-        'message': message,
-        'beds': beds,
-    })
+    if (beds):
+        response['message'] = 'Listing beds'
+        response['beds'] = beds
+    return response
 
 
 # Get a single bed
-@app.get('/bed', tags=["Beds"])
+@app.get('/bed', tags=['Beds'])
 async def get_bed(id: int = Query(None)):
-    message = 'Beds not found'
+    response = {
+        'message': 'Beds not found',
+    }
     bed = beds_controller.get_one(id)
-    if (bed):
-        message = 'Bed found'
-    return ({
-        'message': message,
-        'bed': bed,
-    })
+    if bed:
+        response['message'] = 'Bed found'
+        response['bed'] = bed
+    return response
 
 
 # POST
 # Create  a bed
-@app.post('/bed', tags=["Beds"])
+@app.post('/bed', tags=['Beds'])
 def create_bed(request: list[BedRequestCreate]):
-    message = 'Beds couldn´t be created'
+    response = {
+        'message': 'Beds couldn´t be created',
+    }
     if beds_controller.create(request=request):
-        message = 'Beds created successfully'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Beds created successfully'
+    return response
 
 
 # DELETE
 # Remove a bed
-@app.delete('/bed', tags=["Beds"])
+@app.delete('/bed', tags=['Beds'])
 def delete_bed(id: int):
-    message = 'Bed does not exist'
+    response = {
+        'message': 'Bed does not exist',
+    }
     if beds_controller.delete(id):
-        message = 'Bed successfully deleted'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Bed successfully deleted'
+    return response
 
 
 # PUT
 # Update a bed
-@app.put('/bed', tags=["Beds"])
+@app.put('/bed', tags=['Beds'])
 def update_bed(id: int, request: BedRequestUpdate):
-    message = 'Bed not found'
+    response = {
+        'message': 'Bed not found',
+    }
     if beds_controller.update(id, request):
-        message = 'Bed successfully updated'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Bed successfully updated'
+    return response
 
 
 # Plant endpoints
@@ -197,62 +197,62 @@ def update_bed(id: int, request: BedRequestUpdate):
 
 # GET
 # Get all and multi plants
-@app.get('/plants', tags=["Plants"])
+@app.get('/plants', tags=['Plants'])
 async def get_plants(id: Optional[List[int]] = Query(None)):
-    message = 'Plants not found'
+    response = {
+        'message': 'Plants not found',
+    }
     plants = plants_controller.get_multi(id)
-    if (len(plants) > 0):
-        message = 'Listing plants'
-    return ({
-        'message': message,
-        'plants': plants,
-    })
+    if plants:
+        response['message'] = 'Listing plants'
+        response['plants'] = plants
+    return response
 
 
 # Get a single plant
-@app.get('/plant', tags=["Plants"])
+@app.get('/plant', tags=['Plants'])
 async def get_plant(id: int):
-    message = 'Plant not found'
+    response = {
+        'message': 'Plant not found',
+    }
     plant = plants_controller.get_one(id)
-    if (plant):
-        message = 'Plant found'
-    return ({
-        'message': message,
-        'plants': plant,
-    })
+    if plant:
+        response['message'] = 'Plant found'
+        response['plant'] = plant
+    return response
 
 
 # POST
 # Create a plant
-@app.post('/plant', tags=["Plants"])
+@app.post('/plant', tags=['Plants'])
 def create_plant(request: list[PlantRequestCreate]):
-    message = 'Plants couldn´t be created'
+    response = {
+        'message': 'Plants couldn´t be created',
+    }
     if plants_controller.create(request=request):
-        message = 'Plants created successfully'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Plants created successfully'
+    return response
 
 
 # DELETE
 # Remove a plant
-@app.delete('/plant', tags=["Plants"])
+@app.delete('/plant', tags=['Plants'])
 def delete_plant(id: int):
-    message = 'Plant does not exist'
+    response = {
+        'message': 'Plant does not exist',
+    }
     if plants_controller.delete(id):
-        message = 'Plant successfully deleted'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Plant successfully deleted'
+    return response
 
 
 # Put
 # Update a plant
-@app.put('/plant', tags=["Plants"])
+@app.put('/plant', tags=['Plants'])
 def update_plant(id: int, request: PlantRequestUpdate):
-    message = 'Plant not found'
+    response = {
+        'message': 'Plant not found',
+    }
     if plants_controller.update(id, request):
-        message = 'Plant successfully updated'
-    return ({
-        'message': message,
-    })
+        response['message'] = 'Plant successfully updated'
+    return response
