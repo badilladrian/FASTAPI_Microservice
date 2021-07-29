39b347efe57ea9bc9cba0d56c50d719aed60db88
Carlos Herrera <carloshv93@gmail.com>
2021-07-29 07:19:22 -0600

    Fix: update plant's bed_id

--- MVC/models.py
+++ MVC/models.py
@@ -8,7 +8,7 @@ from sqlalchemy import (
     Column,
     ForeignKey,
     Integer,
-    String
+    String,
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
